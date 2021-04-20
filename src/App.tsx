59b2e1d26a1d24675bba02e5fc7233dbe6ba2b1e
gwardziak <sebaztian60@gmail.com>
2021-04-20 20:13:47 +0200

    add fetch on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Scrollbar } from "react-scrollbars-custom";
+import { ScrollState } from "react-scrollbars-custom/dist/types/types";
 import "./assets/main.css";
 import { Character } from "./components/Character";
 import { SearchBar } from "./components/SearchBar";
@@ -7,61 +9,63 @@ import { useRootStore } from "./stores/RootStore";
 
 const App = observer(() => {
   const rootStore = useRootStore();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     rootStore.peopleStore.fetchAll();
   }, [rootStore]);
 
   return (
-    <section className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4 ">
-      <header className="flex items-center justify-between ">
-        <h2 className="text-3xl leading-10 font-medium text-black">
-          Star Wars Characters Catalogue
-        </h2>
-      </header>
-      <SearchBar />
-      <ul className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4">
-        {rootStore.peopleStore.filter !== ""
-          ? rootStore.peopleStore.filterByName.map((person) => (
-              <Character
-                name={person.name}
-                birthYear={person.birth_year}
-                gender={person.gender}
-                films={person.films}
-                height={person.height}
-              />
-            ))
-          : rootStore.peopleStore.peopleInfo.people.map((person) => (
-              <Character
-                name={person.name}
-                birthYear={person.birth_year}
-                gender={person.gender}
-                films={person.films}
-                height={person.height}
-              />
-            ))}
-      </ul>
-      {rootStore.peopleStore.peopleInfo.hasMore ? (
-        <button
-          onClick={() => {
-            rootStore.peopleStore.fetchPeople();
-          }}
-        >
-          Show more
-        </button>
-      ) : null}
-    </section>
+    <Scrollbar
+      onUpdate={async (scrollValues: ScrollState) => {
+        if (
+          scrollValues.scrollTop + scrollValues.clientHeight >=
+            scrollValues.scrollHeight &&
+          !isFetching &&
+          rootStore.peopleStore.peopleInfo.hasMore &&
+          rootStore.peopleStore.filter === "" &&
+          scrollValues.scrollTop !== 0
+        ) {
+          console.log("Fetching");
+          setIsFetching(true);
+          await rootStore.peopleStore.fetchPeople();
+          setIsFetching(false);
+          console.log("Done fetching");
+        }
+      }}
+    >
+      <section className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4">
+        <header className="flex items-center justify-between">
+          <h2 className="text-3xl leading-10 font-medium text-black">
+            Star Wars Characters Catalogue
+          </h2>
+        </header>
+        <SearchBar />
+
+        <ul className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 mb-4">
+          {rootStore.peopleStore.filter !== ""
+            ? rootStore.peopleStore.filterByName.map((person) => (
+                <Character
+                  name={person.name}
+                  birthYear={person.birth_year}
+                  gender={person.gender}
+                  films={person.films}
+                  height={person.height}
+                />
+              ))
+            : rootStore.peopleStore.peopleInfo.people.map((person) => (
+                <Character
+                  name={person.name}
+                  birthYear={person.birth_year}
+                  gender={person.gender}
+                  films={person.films}
+                  height={person.height}
+                />
+              ))}
+        </ul>
+      </section>
+    </Scrollbar>
   );
 });
 
 export default App;
-
-// {rootStore.peopleStore.peopleInfo.people.map((person) => (
-//   <Character
-//     name={person.name}
-//     birthYear={person.birth_year}
-//     gender={person.gender}
-//     films={person.films}
-//     height={person.height}
-//   />
-// ))}
